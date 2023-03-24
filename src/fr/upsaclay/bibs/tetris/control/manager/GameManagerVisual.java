c8f44ef588ce7431816c35ab6aa3410e383a8cbb
Elora-V <elora95.vigo@gmail.com>
2023-03-24 12:34:13 +0100

    gameplayer

--- src/fr/upsaclay/bibs/tetris/control/manager/GameManagerVisual.java
+++ src/fr/upsaclay/bibs/tetris/control/manager/GameManagerVisual.java
@@ -14,7 +14,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class GameManagerVisual extends AbstractGameManager  {
+public class GameManagerVisual extends AbstractGameManager{
 
     private GameFrame view;
 
@@ -80,7 +80,6 @@ public class GameManagerVisual extends AbstractGameManager  {
                 view.drawManagementView();
                 break;
             case QUIT:
-                //System.exit(0);
                 break;
             default:
                 break;
